afbb101fd8f71e03fb3acccbc6488a68f5ee2ca5
mssung94 <mssung94@gmail.com>
2019-01-25 16:02:15 +0900

    음식 검새과 회원가입 연동기능 추가

--- db/sql.py
+++ db/sql.py
@@ -8,7 +8,7 @@ def loginSql( uid, upw ):
         connection = my.connect(host='localhost', # 디비 주소
                             user='root',      # 디비 접속 계정
                             password='12341234', # 디지 접속 비번
-                            db='python_db',   # 데이터베이스 이름
+                            db='ssmc_project',   # 데이터베이스 이름
                             #port=3306,        # 포트     
                             charset='utf8',
                             cursorclass=my.cursors.DictCursor) # 커서타입지정
@@ -17,12 +17,16 @@ def loginSql( uid, upw ):
             print('DB Open : Login')
             #####################################################
             with connection.cursor() as cursor:
-                sql    = "select * from users where uid=%s and upw=%s;"
+                sql    = '''
+                    SELECT *
+                    FROM users
+                    WHERE uid=%s and upw=%s;
+                '''
                 cursor.execute( sql, (uid, upw) )
 
                 row    = cursor.fetchone()
                 print( row )
-                print( "%s님"  % row['name']  )
+                print( "%s(%s)님이 로그인하였습니다."  % (row['uid'],row['uname'])  )
             #####################################################
     except Exception as e:
         print('->', e)
@@ -33,15 +37,51 @@ def loginSql( uid, upw ):
             print('DB Close : Login Done')
     return row
 
-# main 검색창에서 요청한 음식을 DB로부터 가져오는 함수
-def searchFood(foodname):
+# 회원가입하는 함수
+def signin(info):
+    connection = None
+    result = 0 # 로그인 결과를 담는 변수
+    try:
+        connection = my.connect(host='localhost', # DB 주소
+                                user='root',      # DB 접속 계정
+                                password='12341234', # DB 접속 비번
+                                db='ssmc_project',   # Database 이름
+                                #port=3306,        # Port     
+                                charset='utf8',
+                                cursorclass=my.cursors.DictCursor) # Cursor Type
+
+        if connection:
+            print('user DB OPEN')
+            #####################################################
+            with connection.cursor() as cursor:
+                sql    ='''
+                INSERT INTO users
+                (`uname`,`uid`,`upw`,`age`,`sex`,`weight`,`height`)
+                VALUES
+                (%s,%s,%s,%s,%s,%s,%s)
+                '''
+                cursor.execute( sql,tuple(info.values()) )
+                connection.commit()
+                result = connection.affected_rows() # 1이 되야지 성공
+            #####################################################
+    except Exception as e:
+        print('->', e)
+        result = 0
+    finally:
+        if connection:
+            connection.close()
+            print('user DB CLOSE')
+    return result
+
+# main 검색창에서 요청한 음식을 DB로부터 가져오는 함수 -> ajax에서 쓸 용도
+def searchFoodAjax(foodname):
     connection = None
     row = None # 로그인 결과를 담는 변수
     try:
         connection = my.connect(host='localhost', # DB 주소
                                 user='root',      # DB 접속 계정
                                 password='12341234', # DB 접속 비번
-                                db='python_db',   # Database 이름
+                                db='ssmc_project',   # Database 이름
                                 #port=3306,        # Port     
                                 charset='utf8',
                                 cursorclass=my.cursors.DictCursor) # Cursor Type
@@ -50,9 +90,14 @@ def searchFood(foodname):
             print('DB OPEN : Find Food')
             #####################################################
             with connection.cursor() as cursor:
-                sql    = "select foodname,kcal from food where foodname=%s;"
-                cursor.execute( sql, (foodname,) )
-                row    = cursor.fetchone()  # 하나의 row를 뽑을때
+                sql    = '''
+                    SELECT foodname,kcal
+                    FROM food
+                    WHERE foodname LIKE '%%%s%%'
+                    LIMIT 0, 10;
+                ''' % (foodname)
+                cursor.execute( sql )
+                row    = cursor.fetchall()  # 하나의 row를 뽑을때
             #####################################################
     except Exception as e:
         print('->', e)
@@ -79,4 +124,21 @@ def searchFood(foodname):
 # def userOneMonth(uid):
 
 if __name__ == '__main__':
-    searchFood('음식')
+    # ajax에서 음식 리스트 찾는 연습
+    # print('-'*50)
+    # for flist in searchFoodAjax(input('음식을 검색하세요 : ')):
+    #     print( flist['foodname'],'\t',flist['kcal'],'kcal')
+    # print('-'*50)
+
+    # loginSql('m','1')
+
+    info = {
+        'uname':'multi',
+        'uid':'m',
+        'upw':'1',
+        'age':20,
+        'sex':'m',
+        'height':170,
+        'weight':70
+    }
+    print(signin(info))
