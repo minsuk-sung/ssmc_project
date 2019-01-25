dda95b58a9e388db23a66a9ae7facd603b4786c2
mssung94 <mssung94@gmail.com>
2019-01-25 10:52:06 +0900

    DB로부터 음식을 검색하거나 먹은 음식 찾아주는 함수

--- db/sql.py
+++ db/sql.py
@@ -1,5 +1,6 @@
 import pymysql as my
 
+# 아이디 및 비밀번호 입력받으면 로그인하는 함수 
 def loginSql( uid, upw ):
     connection = None
     row = None # 로그인 결과를 담는 변수
@@ -13,26 +14,15 @@ def loginSql( uid, upw ):
                             cursorclass=my.cursors.DictCursor) # 커서타입지정
 
         if connection:
-            print('디비 오픈')
+            print('DB Open : Login')
             #####################################################
             with connection.cursor() as cursor:
-                # 파라미터 자리를 '' 포함해서 %s로 대체
-                # 'm' => %s 
-                # 방법 1
                 sql    = "select * from users where uid=%s and upw=%s;"
                 cursor.execute( sql, (uid, upw) )
 
-                # 방법 2 : 적절히 섞어서 사용한다(적합한 타이밍이 나온다)
-                # select * from users where uid=m and upw=1;
-                #sql    = "select * from users where uid='%s' and upw='%s';" % ('m', '1')
-                #print( sql )
-                #cursor.execute( sql )
-
-                row    = cursor.fetchone()  # 하나의 row를 뽑을때
-                # {'id': 1, 'name': '멀티', 'uid': 'm', 'upw': '1', 'regdate': datetime.datetime(2019, 1, 7, 14, 14, 23)}
+                row    = cursor.fetchone()
                 print( row )
-                print( "%s님 방갑습니다."  % row['name']  )
-                #cursor.close()
+                print( "%s님"  % row['name']  )
             #####################################################
     except Exception as e:
         print('->', e)
@@ -40,5 +30,53 @@ def loginSql( uid, upw ):
     finally:
         if connection:
             connection.close()
-            print('디비 닫기')
+            print('DB Close : Login Done')
     return row
+
+# main 검색창에서 요청한 음식을 DB로부터 가져오는 함수
+def searchFood(foodname):
+    connection = None
+    row = None # 로그인 결과를 담는 변수
+    try:
+        connection = my.connect(host='localhost', # DB 주소
+                                user='root',      # DB 접속 계정
+                                password='12341234', # DB 접속 비번
+                                db='python_db',   # Database 이름
+                                #port=3306,        # Port     
+                                charset='utf8',
+                                cursorclass=my.cursors.DictCursor) # Cursor Type
+
+        if connection:
+            print('DB OPEN : Find Food')
+            #####################################################
+            with connection.cursor() as cursor:
+                sql    = "select foodname,kcal from food where foodname=%s;"
+                cursor.execute( sql, (foodname,) )
+                row    = cursor.fetchone()  # 하나의 row를 뽑을때
+            #####################################################
+    except Exception as e:
+        print('->', e)
+        row = None
+    finally:
+        if connection:
+            connection.close()
+            print('DB Close : Find Food')
+    return row
+
+# DB에서 모든 정보를 가지고오는 함수
+# def searchFoodNutrient(foodname):
+
+# 사용자가 입력해둔 개인정보를 가져오는 함수 -> 키 / 몸무게 등
+# def searchUserInfo(uid):
+
+# 사용자가 1일간 먹은 음식
+# def userOnePeriod(uid,):
+
+# 사용자가 1주일간 먹은 음식
+# def userOneWeek(uid):
+
+# 사용자가 1달간 먹은 음식
+# def userOneMonth(uid):
+
+if __name__ == '__main__':
+    searchFood('음식')
