db5269061a24a6cc4aed6e4ea90510f4b1561bc1
Ko-jongyoon <rhwhddbs12@naver.com>
2019-01-28 09:22:15 +0900

    웹 로그인,회원가입

--- run.py
+++ run.py
@@ -9,17 +9,11 @@ app.secret_key= 'qweasdzxc'
 
 @app.route('/')
 def main():
-    # 세션이 없으면 로그인으로
-    # if not 'user_id' in session:
-    #     return redirect( url_for( 'login' ) )
+  
     return render_template('main.html')
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login',methods=['POST'])
 def login():
-    if  request.method == 'GET':
-        # GET
-        return render_template('header.html')
-    else:  
          uid = request.form['uid']
          print(uid)
          upw = request.form['upw']
@@ -27,12 +21,40 @@ def login():
          if row:
              # 세션생성
              session['user_id']= uid
-             session['user_nm']= row['name']
+             session['user_nm']= row['uname']
              # 홈페이지로 이동    
              return redirect(url_for('main') )
          else:
             return render_template('error.html', msg='아이디 비번확인 요청')
 
+@app.route('/logout')
+def logout():
+    # 세션 제거
+    if 'user_id' in session:
+        session.pop('user_id',None)
+    if 'user_nm' in session:   
+        session.pop('user_nm',None)
+    # 홈페이지 이동
+    return redirect(url_for('main'))
+
+@app.route('/join')
+def join():
+     return render_template('signin.html')
+
+@app.route('/join2',methods=['POST'])
+def join2():
+    
+    uid = request.form['uid']
+    upw = request.form['upw']
+    uname = request.form['uname']
+    sex = request.form['sex']
+    age = request.form['age']
+    weight = request.form['weight']
+    height = request.form['height']
+    
+    row = signin(uid,upw,uname,age,sex,weight,height)
+    return render_template('signin2.html')
+      
 
 if __name__ == '__main__':# 이코드를 메인으로 구동시 서버가동
     app.run(debug=True)
